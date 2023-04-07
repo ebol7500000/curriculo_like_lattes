22b5a7cb5e0a07a22a7e57fe0aaedbca89ff3c24
Sete unico <90002617+ebol7500000@users.noreply.github.com>
2023-04-07 19:06:02 -0300

    070423
    19:05

---
+++ cv-home/index.php
@@ -0,0 +1,108 @@
+
+
+<!DOCTYPE html><!-- defini html o documento , mas como é em php não
+faz muito sentido. Se quiser retirar -->
+<!--Este é o formato de escrita de comentário de html. coloca se um menor 
+que , depois um interjeição e exatamente dois traços após  . No final também
+é dois traços e um maior que.-->
+<!-- lembrando sempre que é necessário salvar em html para renderizar tudo
+direitinho--> 
+<html><!-- tag html é como um pequeno comando entre maior e menor que.
+Essa renderiza o html lido -->
+<!-- no notepad do windows: você coloca como todos os arquivos e adiciona 
+a extensão (.html)no nome do arquivo.-->
+     <head><!--essa é o cabeçalho chamado por vezes muitas de header, 
+que dá no mesmo. Mas usar header como tag é outro comando. Só ilustrei
+a fins didáticos.-->
+
+<title>cv lattes</title><!-- aqui é o topo da pagina. O que title faz 
+é armazenar as palavras do topo da aba. Cuidado de testar sempre,
+pois há variações no tamanho de aba , de browser para browser(de 
+navegador de internet para navegador de internet-->
+
+<link rel=stylesheet href="./style.css"/><!-- essa tag é para importar 
+textos em css que eu uso. Em especifico a mim. Tem gente que implementa 
+o css no próprio código do html.-->
+<!-- a tag link  tem alguns campos. Dois em específico. rel e href. No
+rel você expressa o tipo de arquivo importado. No caso folha de estilo. 
+E href é o mesmo lá da tag a , de link. Mais pra frente vão entender.-->
+<!--no final sempre coloque o barra invertida na frente da tag: para 
+indicar o fim do código da tag no espaço que você colocou-->
+</head><!-- aqui é um exemplo do ultimo comentário que eu falei. Nesta 
+tag em especifica foi colocada o barra invertido e a tag head(de cabeçalho).
+-->
+
+<body style="background-color:#ccc; " ><!-- Aqui começa o 'corpo' do nosso 
+código --><!--apartir daqui tudo é renderizado. Exceto outros códigos como
+javascript por exemplo.--><!--se vocês abrirem o arquivo vão ver que o texto
+em comentário não é renderizado.-->
+<div class="container">  
+<header>
+
+</header>
+    <div class="clear">  </div>   
+    <div class="setevw" >
+    <div class="clear">  </div>
+	<a href='.././cv-home/index.php'>
+        <div class= "card1">
+             <img  width="100%" height="100%"/>
+        </div></a>
+	<a href='.././cv-formacao/index.php'>
+        <div class= "card2">
+             <img  width="100%" height="100%"/>
+        </div></a>
+        <a href='.././cv-action/index.php'>
+        <div class= "card3">
+             <img  width="100%" height="100%"/>
+        </div></a>
+        <div class= "card4">
+             <img  width="100%" height="100%"/>
+        </div>
+        <div class= "card5">
+             <img  width="100%" height="100%"/>
+        </div>
+        <div class= "card6">
+             <img  width="100%" height="100%"/>
+        </div>
+        <div class= "card7">
+             <img  width="100%" height="100%"/>
+        </div>
+      
+
+
+        <div class= "card8">
+             <img  width="100%" height="100%"/>
+        </div>
+        <div class= "card9">
+             <img  width="100%" height="100%"/>
+        </div>
+        <div class= "card10">
+             <img  width="100%" height="100%"/>
+        </div>
+        <div class= "card11">
+             <img  width="100%" height="100%"/>
+        </div>
+        <div class= "card12">
+             <img  width="100%" height="100%"/>
+        </div>
+        <div class= "card13">
+             <img  width="100%" height="100%"/>
+        </div>
+       
+        <div class= "card14">
+             <img  width="100%" height="100%"/>
+        </div>
+
+
+
+    </div>
+
+    
+
+
+</div>
+	</body>
+
+
+
+</html>
